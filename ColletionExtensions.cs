8c45bad574869d2291e74ffea2847c14e782efe2
Asim <gamedevelopementasim@gmail.com>
2022-02-12 17:05:59 +0800

    update collection exntesion
    ienumberable shuffle, and shuffle list

---
+++ ColletionExtensions.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace asim.extensions
+{
+    /// <summary>
+    /// Extension class for everything to do with collections, list, dictionarys etc..
+    /// </summary>
+    public static class ColletionExtensions
+    {
+        /// <summary>
+        /// Fisher-Yates shuffle using unity random range
+        /// </summary>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = Random.Range(0, n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+        /// <summary>
+        /// IEnumerable version of Fisher-Yates shuffle using unity random range
+        /// </summary>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            T[] s = source.ToArray();
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                int swapIndex = Random.Range(0, i + 1);
+                yield return s[swapIndex];
+                s[swapIndex] = s[i];
+            }
+        }
+    }
+}
