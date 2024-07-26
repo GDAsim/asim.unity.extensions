f3273193e96a1de2810d08f543fe18412c4dccee
GDAsim <gamedevelopementasim@gmail.com>
2024-07-26 09:46:26 +0800

    add List Extensions, Math Extensions, modify Vector2,Vector3

--- AssetsExtentions.cs
+++ AssetsExtentions.cs
@@ -2,28 +2,28 @@
 
 namespace asim.unity.extensions
 {
-	/// <summary>
-	/// Extension Class for Unity Assets/StreamingAssets/Resources etc..
-	/// </summary>
-	public static class AssetsExtentions
-	{
-		/// <summary>
-		/// Read a text asset and return it as lines of strings
-		/// </summary>
-		public static string[] ReadLinesTextAsset(TextAsset textasset)
-		{
-			string[] tilemapstring = textasset.text.Split('\n');
+    /// <summary>
+    /// Extension Class for Unity Assets/StreamingAssets/Resources etc..
+    /// </summary>
+    public static class AssetsExtentions
+    {
+        /// <summary>
+        /// Read a text asset and return it as lines of strings
+        /// </summary>
+        public static string[] ReadLinesTextAsset(TextAsset textasset)
+        {
+            string[] tilemapstring = textasset.text.Split('\n');
 
-			return tilemapstring;
-		}
+            return tilemapstring;
+        }
 
-		/// <summary>
-		/// Read a text asset and return it as lines of char array
-		/// </summary>
-		public static char[][] ReadCharArrayTextAsset(TextAsset textasset)
-		{
-			string[] tilemapstring = ReadLinesTextAsset(textasset);
-			return TextExtension.StringArrayToCharArray(tilemapstring);
-		}
-	}
+        /// <summary>
+        /// Read a text asset and return it as lines of char array
+        /// </summary>
+        public static char[][] ReadCharArrayTextAsset(TextAsset textasset)
+        {
+            string[] tilemapstring = ReadLinesTextAsset(textasset);
+            return TextExtension.StringArrayToCharArray(tilemapstring);
+        }
+    }
 }
--- Vector3Extensions.cs
+++ Vector3Extensions.cs
@@ -42,5 +42,33 @@ namespace asim.unity.extensions
             }
             return v;
         }
+
+        //====================================================================================================
+        //====================================================================================================
+
+        /// <summary>
+        /// http://answers.unity3d.com/questions/131624/vector3-comparison.html#answer-131672
+        /// Returns true if vector are equal with a angle error margin
+        /// </summary>
+        public static bool ApproxEqual(this Vector3 v1, Vector3 v2, float angleError = 0.001f)
+        {
+            //if they aren't the same length, don't bother checking the rest.
+            if (!Mathf.Approximately(v1.magnitude, v2.magnitude))
+                return false;
+
+            var cosAngleError = Mathf.Cos(angleError * Mathf.Deg2Rad);
+
+            //A value between -1 and 1 corresponding to the angle.
+            //The dot product of normalized Vectors is equal to the cosine of the angle between them.
+            //So the closer they are, the closer the value will be to 1. Opposite Vectors will be -1
+            //and orthogonal Vectors will be 0.
+            var cosAngle = Vector3.Dot(v1.normalized, v2.normalized);
+
+            //If angle is greater, that means that the angle between the two vectors is less than the error allowed.
+            return cosAngle >= cosAngleError;
+        }
+
+        //====================================================================================================
+        //====================================================================================================
     }
 }
